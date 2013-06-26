e5a21fdd01696cdaa9964561370d1492c13b671a
Alexandre Passos <alexandre.tp@gmail.com>
2013-06-26 13:09:45 -0400

    cleaning up the parallel kmeans

--- src/main/scala/cc/refectorie/Encoding/ParallelKMeans.scala
+++ src/main/scala/cc/refectorie/Encoding/ParallelKMeans.scala
@@ -1,6 +1,7 @@
 package cc.refectorie.Encoding
 
 import java.util.Random
+import cc.factorie.la.{DenseTensor1, Tensor1, Tensor}
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,58 +11,46 @@ import java.util.Random
  * To change this template use File | Settings | File Templates.
  */
 
-class ParallelKMeans(data: Array[Array[(Int, Double)]], k: Int,  d: Int) {
+/**
+ * An implementation of kmeans using parallel collections
+ * @param data The data points to be clustered
+ * @param k The number of clusters
+ * @param d The dimensionality of the data
+ */
+class ParallelKMeans(data: Seq[Tensor1], k: Int,  d: Int) {
 
-  val means = Array.ofDim[Array[Double]](k)
-  for (i <- 0 until k) means(i) = Array.ofDim[Double](d)
-  val norms = Array.ofDim[Double](k)
+  val means = Array.fill(k)(new DenseTensor1(d))
+  val norms = Array.fill(k)(0.0)
 
   val rng = new java.util.Random()
 
-  def sparseDot(d: Array[(Int,  Double)], m: Array[Double]) = {
-    var dot = 0.0
-    var j = 0
-    while (j < d.length) {
-      dot += m(d(j)._1)*d(j)._2
-      j += 1
-    }
-    dot
-  }
   // (x-y)^2 = x^2 - 2xy + y^2
   // note that x^2 is constant, so we can ignore it
-  def dist(d: Array[(Int,  Double)], m: Int) = norms(m) - 2*sparseDot(d, means(m))
-
-
-  def addTo(v: Array[Double], x: Array[(Int, Double)]) { x.foreach(e => v(e._1) += e._2) }
+  def dist(d: Tensor, m: Int) = norms(m) - 2* d.dot(means(m))
 
   def initialize() {
     for (i <- 0 until k) {
-      addTo(means(i), data(rng.nextInt(data.length)))
-      norms(i) = means(i).foldLeft(0.0)((sum, element) => sum + element*element)
+      means(i) += data(rng.nextInt(data.length))
+      norms(i) = means(i).twoNormSquared
     }
   }
 
   def eStep() = {
     data.par.map(d => {
       (0 until k).map(i => (i,  dist(d, i))).minBy(a => a._2)
-    }).seq.toArray
+    }).seq
   }
 
-  def mStep(e: Array[(Int,Double)]) = {
-    //e.zipWithIndex.groupBy(f =>  f._1._1).map(group => {
+  def mStep(e: Seq[(Int,Double)]) = {
     e.zipWithIndex.par.groupBy(f => f._1._1).map(group => {
       val mean = group._1
       val examples = group._2
       val newMean = means(mean)
-      var i = 0
-      while (i < newMean.length) {newMean(i) = 0.0; i+=1}
-      examples.foreach(e => addTo(newMean, data(e._2)))
+      newMean.zero()
+      examples.foreach(e => newMean += data(e._2))
       val error = examples.map(_._1._2).sum
-      while (i < newMean.length) {
-        newMean(i) /= examples.length
-        i += 1
-      }
-      norms(mean) = newMean.foldLeft(0.0)((sum, e) => sum + e*e)
+      newMean /= examples.length
+      norms(mean) = newMean.twoNormSquared
       error
     }).sum
   }
@@ -70,7 +59,7 @@ class ParallelKMeans(data: Array[Array[(Int, Double)]], k: Int,  d: Int) {
     var err = 1000000000.0
     var oldError = Double.PositiveInfinity
     var i = 0
-    while ((i < iterations) && (oldError - err > tolerance)) {
+    while (i < iterations && oldError - err > tolerance) {
       oldError = err
       err = mStep(eStep())
       i += 1
@@ -81,23 +70,8 @@ class ParallelKMeans(data: Array[Array[(Int, Double)]], k: Int,  d: Int) {
 }
 
 object ParallelKMeans {
-  def main(args: Array[String]) {
-    val rng = new java.util.Random()
-    val data = Array.ofDim[Array[(Int, Double)]](200)
-    for (i <- 0 until data.length) {
-      data(i) = Array.ofDim[(Int,  Double)](20)
-      for (j <- 0 until data(i).length) {
-        data(i)(j) = (rng.nextInt(100), rng.nextGaussian())
-      }
-    }
-
-    val km = new ParallelKMeans(data, 20, 100)
-    km.initialize()
-    km.process(30)
-  }
-
   // Runs the parallel KMeans n times and returns the means of the best run
-  def bestOfN(data: Array[Array[(Int,  Double)]], k: Int,  n: Int,  d: Int, iterations: Int) = {
+  def bestOfN(data: Seq[Tensor1], k: Int,  n: Int,  d: Int, iterations: Int) = {
     var bestKM: ParallelKMeans = null
     var bestErr = Double.PositiveInfinity
     for (i <- 0 until n) {
